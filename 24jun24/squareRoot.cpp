753525aec1ff735320e5e620820341f2c71775dc
Rushikesh <Rishikale32@gmail.com>
2024-06-24 09:33:00 +0530

    done

---
+++ 24jun24/squareRoot.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here  
+        long long i =1;
+        long long num = x;
+        long long cnt =0;
+        
+        while (num>=i){
+            num = num -i;
+            i+=2;
+            cnt++;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
