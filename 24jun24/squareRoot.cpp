9a774a7e01b02894340637c7abce6c38b10006b0
Rushikesh <Rishikale32@gmail.com>
2024-06-26 06:55:16 +0530

    done

--- 24jun24/squareRoot.cpp
+++ 24jun24/squareRoot.cpp
@@ -2,53 +2,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-  
-
 // } Driver Code Ends
-// Function to find square root
-// x: element to find square root
-class Solution{
-  public:
-    long long int floorSqrt(long long int x) 
-    {
-        // Your code goes here   
-        long long low = 1;
-        long long high =x;
-        long long mid ;
-        long long int sqrt;
-        if (x ==1 || x==2 || x==3) return 1;
-        if (x==0) return 0;
-        
-        while (low<=high) {
-            mid = low +(high -low)/2;
 
-            if (mid*mid == x) return mid;
-            // go to left 
-            if (mid*mid > x) high = mid -1;
-            // go to right
-            else if (mid*mid < x) {
-                low = mid +1;
-                sqrt = mid;
-            }
-        }
-        return sqrt;
-    }
+
+#include <math.h>
+class Solution{
+	public:
+	int NthRoot(int n, int m)
+	{
+	    int low =1;
+	    int high = m;
+	    int mid;
+	    
+	    while (low<=high){
+	        mid = low + (high -low)/2;
+	        if (pow(mid,n) == m) return mid;
+	        
+	        if (pow(mid,n)> m) high = mid-1;
+	        else {
+	            low =mid+1;
+	        }
+	    }
+	    return -1;
+	}  
 };
 
 //{ Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		long long n;
-		cin>>n;
-		Solution obj;
-		cout << obj.floorSqrt(n) << endl;
-	}
-    return 0;   
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
 }
-
 // } Driver Code Ends
