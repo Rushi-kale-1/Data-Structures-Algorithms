0beb0493ba0587bbe1e35aaa04292b19dd3b5772
Rushikesh <Rishikale32@gmail.com>
2024-07-13 08:00:19 +0530

    done

---
+++ 13Jul 2024/longestPrefix.cpp
@@ -0,0 +1,22 @@
+// solution number 1
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        int n = 0;
+        while (n<strs[0].size()){
+        bool add=true;
+        char comp= strs[0][n];
+        for (auto s:strs){
+            if (comp!=s[n]) add = false ;
+        }
+        if(add == true) ans+=comp;
+        else return ans;
+        n++;
+        }
+        return ans;
+    }
+};
+
+// 
